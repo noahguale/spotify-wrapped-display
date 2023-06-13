f7ae623d50034d0b5e5ee813a328320c10ff81ad
Noah Guale <noahguale@gmail.com>
2023-06-13 13:10:12 -0500

    css changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <video className="w-auto h-auto" src="/spotify-wrapped.mov" autoPlay loop muted />
+    <div className="flex items-center justify-center h-screen bg-[#191414]">
+      <video className="w-auto h-auto rounded-[40px]" src="/spotify-wrapped.mov" autoPlay loop muted />
     </div>
   );
 };
