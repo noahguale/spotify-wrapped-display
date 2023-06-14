9b1c3f62a5a1afcf0d5534142bec1bb1ec0f7e5a
Noah Guale <noahguale@gmail.com>
2023-06-14 01:28:04 -0500

    added iphone mockup

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
         <>
             <div className="flex flex-col my-10 justify-center items-center space-y-2">
                 <Spotify height="100px" width=""/>
-                <div className="font-gotham text-[#1DB954] text-[50px] w-full font-semibold">Noah&apos;s Wrapped</div>
+                {/* <div className="font-gotham text-[#1DB954] text-[50px] w-full font-semibold">Noah&apos;s Wrapped</div> */}
             </div>
         
         
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import { Footer } from './Footer';
 import { Header } from './Header';
-
+import Phone from './Phone';
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
       <Header />
-      <video className="w-[360px] h-[640px] rounded-[40px]" src="/spotify-wrapped.mov" autoPlay loop muted />
+      <div className="relative">
+        <Phone />
+        <video className="absolute top-24 left-9 w-[340px] h-[620px] rounded-[10px] " src="/spotify-wrapped.mov" autoPlay loop muted />
+      </div>  
+
+      {/* <video className="w-[360px] h-[640px] rounded-[40px]" src="/spotify-wrapped.mov" autoPlay loop muted /> */}
       <Footer />
 
     </div>
